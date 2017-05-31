55b2840a8fe66427e68b3f64d39e596eefb2ef84
team3-Ngocduc <nguyenngocduc597@gmail.com>
2017-05-31 15:24:02 +0700

    include

---
+++ js/footer.js
@@ -0,0 +1,13 @@
+document.write('\
+\
+    <div class="footer" style="background-color: #292929">\
+        <div class="col-md-7 cl">Facebook | Youtube | Google+</div>\
+            <div class="col-md-5 cr">\
+                Contact:<br>\
+                Phone: 0935-xxx-xxx<br>\
+                Email: xxxxxxxx@gmail.com<br>\
+            </div>\
+        </div>\
+    </div>\
+\
+');
---
+++ js/header.js
@@ -0,0 +1,105 @@
+document.write('\
+                    <!-- Header-->\
+    <div class="container-fluid">\
+        <div class="row">\
+            <div class="col-md-12">\
+                <div class="row">\
+                    <ul class="breadcrumb">\
+                        <li><a href="#">Sign in</a></li>\
+                        <li><a href="#">Sign up</a></li>\
+                    </ul>\
+                </div>\
+            </div>\
+        </div>\
+    </div>\
+                    <!-- Menu -->\
+    <div class="container-fluid">\
+        <div class="row"></div>\
+            <nav class="navbar" data-spy="affix" data-offset-top="25">\
+                <div class="container-fluid">\
+                    <div class="navbar-header">\
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">\
+                            <span class="icon-bar"></span>\
+                            <span class="icon-bar"></span>\
+                            <span class="icon-bar"></span>\
+                        </button>\
+                        <a href="HomePage.html" ><img src="image/name.png" height="45" "/></a>\
+                    </div>\
+                    <div class="collapse navbar-collapse nav navbar-nav navbar-right" id="myNavbar">\
+                        <ul class="nav navbar-nav">\
+                            <li><a href="#">Home</a></li>\
+                            <li><a href="#">Sell</a></li>\
+                            <li><a href="#">About Us</a></li>\
+                            <li><a href="#">Contact Us</a></li>\
+                            <li class="dropdown">\
+                                <a data-toggle="dropdown" href="">Product Catalog<span class="caret"></span></a>\
+                                <ul class="dropdown-menu">\
+                                    <li><a href="DoGiaDung-ProductsPage.html">Đồ gia dụng</a></li>\
+                                    <li><a href="DoDienTu-ProductsPage.html">Đồ điện tử</a></li>\
+                                    <li><a href="DoVanPhong-ProductsPage.html">Đồ văn phòng</a></li>\
+                                    <li><a href="LamDep-ProductsPage.html">Làm đẹp</a></li>\
+                                </ul>\
+                            </li>\
+                        </ul>\
+                    </div>\
+                </div>\
+            </nav>\
+    </div>\
+                    <!-- banner -->\
+    <div class="container-fluid">\
+        <div id="myCarousel" class="carousel slide" data-ride="carousel">\
+            <!-- Indicators\
+            <ol class="carousel-indicators">\
+                <li data-target="#myCarousel" data-slide-to="0" class="active"></li>\
+                <li data-target="#myCarousel" data-slide-to="1"></li>\
+                <li data-target="#myCarousel" data-slide-to="2"></li>\
+            </ol>-->\
+\
+                    <!-- Wrapper for slides -->\
+            <div class="carousel-inner">\
+\
+                <div class="item active">\
+                    <img src="image/cho1.jpg" alt="spdd" style="width:1200px;height: 450px">\
+                    <div class="carousel-caption">\
+                        <h1 style="text-align: right">Sản phẩm đa dạng</h1>\
+                        <p style="text-align: right">Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió\
+                            Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió\
+                            Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió </p>\
+                    </div>\
+                </div>\
+\
+                <div class="item">\
+                    <img src="image/cho2.jpg" alt="ddgd" style="width:1200px;height: 450px">\
+                    <div class="carousel-caption">\
+                        <h1 style="text-align: right">Dễ dàng giao dịch</h1>\
+                        <p style="text-align: right">Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió\
+                            Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió\
+                            Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió </p>\
+                    </div>\
+                </div>\
+\
+                <div class="item">\
+                    <img src="image/cho3.jpg" alt="at" style="width:1200px;height: 400px">\
+                    <div class="carousel-caption">\
+                        <h1 style="text-align: right">An toàn trong mua bán</h1>\
+                        <p style="text-align: right">Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió\
+                            Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió\
+                            Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió Chém gió </p>\
+                    </div>\
+                </div>\
+\
+            </div>\
+\
+                        <!-- Left and right controls\
+                <a class="left carousel-control" href="#myCarousel" data-slide="prev">\
+                    <span class="glyphicon glyphicon-chevron-left"></span>\
+                    <span class="sr-only">Previous</span>\
+                </a>\
+                <a class="right carousel-control" href="#myCarousel" data-slide="next">\
+                    <span class="glyphicon glyphicon-chevron-right"></span>\
+                    <span class="sr-only">Next</span>\
+                </a>\
+            </div>\
+        </div>-->\
+    </div>\
+');
