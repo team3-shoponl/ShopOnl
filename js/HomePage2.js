cc6298642ed8bbecc913e161108120efda088cf6
Truong Ha Vu <cyberl1772002@gmail.com>
2017-06-07 15:41:41 +0700

    trang san pham

--- js/HomePage2.js
+++ js/HomePage2.js
@@ -2,3 +2,14 @@
  * Created by My PC on 06/06/2017.
  */
 
+/* SideBar */
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+/* Search */
+
