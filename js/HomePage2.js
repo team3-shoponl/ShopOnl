64326a67e494a10fa1fa152836810fe0cad8b58e
Truong Ha Vu <cyberl1772002@gmail.com>
2017-06-11 20:37:14 +0700

    fix tiep

--- js/HomePage2.js
+++ js/HomePage2.js
@@ -3,13 +3,7 @@
  */
 
 /* SideBar */
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-}
 
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
 
 /* Search */
 
